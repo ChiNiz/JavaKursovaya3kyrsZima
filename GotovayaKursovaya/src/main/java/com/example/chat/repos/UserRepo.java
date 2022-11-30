91c0efa72df8ba8c0f31614254c889aaa1a93b8d
ChiNiz <90142585+ChiNiz@users.noreply.github.com>
2022-12-01 01:26:15 +0300

    Delete GotovayaKursovaya directory

--- GotovayaKursovaya/src/main/java/com/example/chat/repos/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.chat.repos;
-
-import com.example.chat.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
