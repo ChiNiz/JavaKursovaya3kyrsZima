cdb5ca4fe4e76140c8c4826ad049211a8634d0c1
ChiNiz <90142585+ChiNiz@users.noreply.github.com>
2022-12-01 01:27:24 +0300

    Add files via upload

---
+++ GotovayaKursovaya/src/main/java/com/example/chat/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.chat.repos;
+
+import com.example.chat.model.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
